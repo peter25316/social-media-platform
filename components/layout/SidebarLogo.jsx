c01a6635a68013a97de7eab922a1ec61e1f7a49d
Peter Huynh <peterlonghaihuynh25@gmail.com>
2024-05-05 13:39:50 -0400

    create MongoDb Atlas Database

--- components/layout/SidebarLogo.jsx
+++ components/layout/SidebarLogo.jsx
@@ -5,7 +5,7 @@ const SidebarLogo = () => {
   const router = useRouter();
 
   return (
-    <div className="rounded-full h-14 w-14 p-4 flex-center justify-center hover:bg-blue-300 cursor-pointer transition">
+    <div className="rounded-full h-14 w-14 p-4 flex-center justify-center hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer transition">
       <BsTwitter size={28} color="white" />
     </div>
   );
