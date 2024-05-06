a0560f0641401f24394f59a856fd86ce37b1bfcd
Peter Huynh <peterlonghaihuynh25@gmail.com>
2024-05-06 17:08:13 -0400

    Image Upload Feature

--- components/modals/EditModal.jsx
+++ components/modals/EditModal.jsx
@@ -8,6 +8,7 @@ import toast from "react-hot-toast";
 
 import Modal from "../Modal";
 import Input from "../Input";
+import ImageUpload from "../ImageUpload";
 
 const EditModal = () => {
   const { data: currentUser } = useCurrentUser();
@@ -59,17 +60,29 @@ const EditModal = () => {
       setIsLoading(false);
     }
   }, [
-    bio,
+    editModal,
     name,
     username,
+    bio,
+    mutateFetchedUser,
     profileImage,
     coverImage,
-    editModal,
-    mutateFetchedUser,
   ]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
+      <ImageUpload
+        value={profileImage}
+        disabled={isLoading}
+        onChange={(image) => setProfileImage(image)}
+        label="Upload profile image"
+      />
+      <ImageUpload
+        value={coverImage}
+        disabled={isLoading}
+        onChange={(image) => setCoverImage(image)}
+        label="Upload cover image"
+      />
       <Input
         placeholder="Name"
         onChange={(e) => setName(e.target.value)}
