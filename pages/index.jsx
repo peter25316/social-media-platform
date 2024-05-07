134dce1b7c99e955df577f73b0b23ff2d0a66a37
Peter Huynh <peterlonghaihuynh25@gmail.com>
2024-05-07 13:18:20 -0400

    create Tweet Form for Tweeting feature

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
 import Header from "@/components/Header";
+import Form from "@/components/Form";
+import PostFeed from "@/components/posts/PostFeed";
 
 const Home = () => {
   return (
     <>
       <Header label="Home" />
+      <Form placeholder="What's happening" />
+      <PostFeed />
     </>
   );
 };
